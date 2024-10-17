39c4392fb2c7520c63f823b4f202d34dcdee765b
unknown <gdutt2304@gmail.com>
2024-10-17 18:07:36 +0530

    Update code 2

--- JavaScript/Code2.js
+++ JavaScript/Code2.js
@@ -1,49 +1,105 @@
-function fun() {
-  alert("Hello!");
-}
+// function fun() {
+//   alert("Hello!");
+// }
+
+// // fun();
+
+// // var a1 = 1;
+// // var a = "String";
+// // var a = 40;
+// // var a = 50;
+// // a = 78;
+// // document.writeln(a);
+
+// // let b = 10;
+// // b = "String";
+
+// // let b = 56;
+
+// // let $ = 50;
+// // document.writeln($);
+
+// // let b = 40;
+// // let b = 50;
+// // document.writeln(b);
+
+// // let a = 10;
+// // let A = 45;
+
+// // function fun() {
+// //   // let
+// // }
+
+// // let a = 20;
+// // a = "String";
+
+// // const c = 40;
+// // c = "Number";
+// // document.writeln(c);
+
+// var ab = 10;
+// var ab = "67";
+// document.writeln(ab);
+
+// let ba = 20;
+// let ba1 = "20";
+// document.writeln(ba);
+
+// const a = 40;
+// document.writeln(a);
+
+// {
+//   let value = 100000;
+// }
+// document.writeln(value);
+
+// {
+//   var value = 100000;
+// }
+// document.writeln(value);
+
+// function fun() {
+//   let A = 40;
+//   document.writeln(A);
+// }
 
 // fun();
 
-// var a1 = 1;
-// var a = "String";
-// var a = 40;
-// var a = 50;
-// a = 78;
-// document.writeln(a);
+// function f1() {
+//   document.writeln("i am function f1");
+// }
+// function f2() {
+//   f1();
+//   f1();
+//   document.writeln("i am function f2");
+// }
+// function f3() {
+//   document.writeln("i am function f3");
 
-// let b = 10;
-// b = "String";
-
-// let b = 56;
-
-// let $ = 50;
-// document.writeln($);
-
-// let b = 40;
-// let b = 50;
-// document.writeln(b);
-
-// let a = 10;
-// let A = 45;
-
-// function fun() {
-//   // let
+//   f2();
 // }
 
-// let a = 20;
-// a = "String";
+// f3();
 
-// const c = 40;
-// c = "Number";
-// document.writeln(c);
+// f3, f2,
+// f2 , f1 , f1
 
-var ab = 10;
-var ab = "67";
-document.writeln(ab);
+// function fun2() {
+//   let a = 2000;
+//   document.writeln(a);
+// }
+function fun2(a, b) {
+  return a + b;
+}
 
-let ba = 20;
-let ba1 = "20";
-document.writeln(ba);
+// let a = 89;
+// let b = 87;
+let a = Number(prompt("Enter your first value"));
+let b = Number(prompt("Enter your second value"));
+// let b = 87;
 
-const a = 40;
-document.writeln(a);
+document.writeln(fun2(a, b));
+// let callingFun2 = fun2(45, 89);
+// document.writeln(callingFun2);
+
+// fun2();
