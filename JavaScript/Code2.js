db83ca4abea79c39e4e4a174bf8e1f32674b50cb
unknown <gdutt2304@gmail.com>
2024-10-17 17:25:19 +0530

    Update js

---
+++ JavaScript/Code2.js
@@ -0,0 +1,49 @@
+function fun() {
+  alert("Hello!");
+}
+
+// fun();
+
+// var a1 = 1;
+// var a = "String";
+// var a = 40;
+// var a = 50;
+// a = 78;
+// document.writeln(a);
+
+// let b = 10;
+// b = "String";
+
+// let b = 56;
+
+// let $ = 50;
+// document.writeln($);
+
+// let b = 40;
+// let b = 50;
+// document.writeln(b);
+
+// let a = 10;
+// let A = 45;
+
+// function fun() {
+//   // let
+// }
+
+// let a = 20;
+// a = "String";
+
+// const c = 40;
+// c = "Number";
+// document.writeln(c);
+
+var ab = 10;
+var ab = "67";
+document.writeln(ab);
+
+let ba = 20;
+let ba1 = "20";
+document.writeln(ba);
+
+const a = 40;
+document.writeln(a);
