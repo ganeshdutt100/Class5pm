08ba9cd6080171a97f8f9256397d78630ea65293
unknown <gdutt2304@gmail.com>
2024-12-12 18:29:37 +0530

    Update Components folder

--- React/code2/src/App.jsx
+++ React/code2/src/App.jsx
@@ -1,5 +1,20 @@
+import Button from "./components/Button";
+
 function App() {
-  return <h1>Lorem ipsum dolor sit amet.</h1>;
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et a omnis
+        quasi numquam sint obcaecati. Sunt labore, nesciunt dignissimos possimus
+        eius eos consectetur magnam rem ipsa corrupti explicabo inventore?
+        Accusamus?
+      </p>
+      {/* <button>Click me </button> */}
+      <Button />
+    </div>
+  );
 }
 
 export default App;
---
+++ React/code2/src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <div>clik me</div>;
+};
+
+export default Button;
