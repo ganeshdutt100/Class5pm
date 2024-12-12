c3cc0592b10bd1b99b0f2bef7c6b0842d6cec889
unknown <gdutt2304@gmail.com>
2024-12-12 18:02:59 +0530

    Update React Class

---
+++ React/code2/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Lorem ipsum dolor sit amet.</h1>;
+}
+
+export default App;
---
+++ React/code2/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import Ganesh from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("ganesh")).render(<Ganesh />);
